dcb5b67ad3c8fae58ef72885af3bc205d588121d
Sebastian Huus <54155988+sebastianhuus@users.noreply.github.com>
2025-08-31 16:22:20 +0200

    Sketch more functions I want to have in the script.
    - I want a function that can add a profile to the local config on the PC.
    - I want a different function for setting the profile based on what is in the PC
    - I also want to be able to list all the profiles that are on the PC.

--- colored-repo.zsh
+++ colored-repo.zsh
@@ -2,7 +2,21 @@ vscode_folder=".vscode"
 settings_file="$vscode_folder/Settings.json"
 
 add_profile() {
-    echo $profile_string >> $settings_file
+    # add profile to local config on computer and save it
+    # needs a name, foreground color, background color
+    # [name]
+    # foregroundcolor = x
+    # backgroundcolor = y
+}
+
+# list all the saved profiles from computer. use ~/.colored-repo-profiles
+list_profiles() {
+
+}
+
+set_profile() {
+    # sets the profile of current repo to some existing profiles
+    # should use gum select or gum filter
 }
 
 init() {
